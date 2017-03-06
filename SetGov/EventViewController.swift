658eb54262e685e3b0043ef4ded61334bdff6365
FrancisZamora <franciszamor@gmail.com>
2017-03-06 14:54:23 -0500

    added SetGovTableView   Class

---
+++ SetGov/EventViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EventViewController.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 3/6/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventViewController: SetGovTableViewController {
+
+}
---
+++ SetGov/SetGovTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SetGovTableViewController.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 3/6/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SetGovTableViewController: UITableViewController {
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+           
+    func setupAppearanceAndRefreshControl() {
+    self.tableView.backgroundColor = SG_NEARLY_WHITE_COLOR
+    self.view.backgroundColor = SG_NEARLY_WHITE_COLOR
+
+    }
+        
+    
+    
+}
