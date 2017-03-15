fb7b8b0237fd8d95369b9bb58781e6d98c5eeeb1
FrancisZamora <franciszamor@gmail.com>
2017-03-14 20:51:55 -0400

    resized pics and moved logic

--- SetGov/EventViewController.swift
+++ SetGov/EventViewController.swift
@@ -24,6 +24,41 @@ class EventViewController: SetGovTableViewController{
 
         print("Table View for Events")
     }
+    
+    func sizeUp() {
+        self.EventCell.hashtagOne.layer.backgroundColor  = SG_RED_COLOR.cgColor
+        self.EventCell.hashtagTwo.layer.backgroundColor = SG_RED_COLOR.cgColor
+        self.EventCell.hashtagTwo.layer.cornerRadius = self.EventCell.hashtagTwo.frame.height/2
+        self.EventCell.hashtagOne.layer.cornerRadius = self.EventCell.hashtagOne.frame.height/2
+        self.EventCell.hashtagTwo.layer.masksToBounds = true
+        self.EventCell.hashtagOne.clipsToBounds = true
+        self.EventCell.hashtagOne.layer.masksToBounds = true
+        self.EventCell.profileImage1.layer.cornerRadius = self.EventCell.profileImage1.frame.height / 2
+        self.EventCell.profileImage1.clipsToBounds = true
+        self.EventCell.profileImage1.layer.borderWidth = 1.5
+        self.EventCell.profileImage1.layer.borderColor = UIColor(red:0.18, green:0.26, blue:0.35, alpha:1.0).cgColor
+        self.EventCell.profileImage2.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
+        self.EventCell.profileImage2.clipsToBounds = true
+        self.EventCell.profileImage2.layer.borderWidth = 1.5
+        self.EventCell.profileImage2.layer.borderColor = UIColor(red:0.18, green:0.26, blue:0.35, alpha:1.0).cgColor
+        self.EventCell.profileImage3.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
+        self.EventCell.profileImage3.clipsToBounds = true
+        self.EventCell.profileImage3.layer.borderWidth = 1.5
+        self.EventCell.profileImage3.layer.borderColor = UIColor(red:0.18, green:0.26, blue:0.35, alpha:1.0).cgColor
+        self.EventCell.profileImage4.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
+        self.EventCell.profileImage4.clipsToBounds = true
+        self.EventCell.profileImage4.layer.borderWidth = 1.5
+        self.EventCell.profileImage4.layer.borderColor = UIColor(red:0.18, green:0.26, blue:0.35, alpha:1.0).cgColor
+        self.EventCell.profileImage5.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
+        self.EventCell.profileImage5.clipsToBounds = true
+        self.EventCell.profileImage5.layer.borderWidth = 1.5
+        self.EventCell.profileImage5.layer.borderColor = UIColor(red:0.18, green:0.26, blue:0.35, alpha:1.0).cgColor
+        self.EventCell.profileImage6.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
+        self.EventCell.profileImage6.clipsToBounds = true
+        self.EventCell.profileImage6.layer.borderWidth = 1.5
+        self.EventCell.profileImage6.layer.borderColor = UIColor(red:0.18, green:0.26, blue:0.35, alpha:1.0).cgColor
+        self.EventCell.profileImage6.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
+    }
 
     
     
@@ -50,52 +85,7 @@ class EventViewController: SetGovTableViewController{
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        self.EventCell =  tableView.dequeueReusableCell(withIdentifier: "EventCell", for:indexPath) as! EventCell
-       self.EventCell.hashtagOne.layer.backgroundColor  = SG_RED_COLOR.cgColor
-       self.EventCell.hashtagTwo.layer.backgroundColor = SG_RED_COLOR.cgColor
-       self.EventCell.hashtagTwo.layer.cornerRadius = self.EventCell.hashtagTwo.frame.height/3
-       self.EventCell.hashtagOne.layer.cornerRadius = self.EventCell.hashtagOne.frame.height/3
-       self.EventCell.hashtagTwo.layer.masksToBounds = false
-       self.EventCell.profileImage1.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
-       self.EventCell.profileImage1.clipsToBounds = true
-       self.EventCell.profileImage1.layer.borderWidth = 3.0
-       self.EventCell.profileImage1.layer.borderColor = UIColor(red:0.71, green:0.10, blue:0.11, alpha:1.0).cgColor
-       self.EventCell.profileImage2.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
-       self.EventCell.profileImage2.clipsToBounds = true
-       self.EventCell.profileImage2.layer.borderWidth = 3.0
-       self.EventCell.profileImage2.layer.borderColor = UIColor(red:0.71, green:0.10, blue:0.11, alpha:1.0).cgColor
-       self.EventCell.profileImage3.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
-       self.EventCell.profileImage3.clipsToBounds = true
-       self.EventCell.profileImage3.layer.borderWidth = 3.0
-       self.EventCell.profileImage3.layer.borderColor = UIColor(red:0.71, green:0.10, blue:0.11, alpha:1.0).cgColor
-       self.EventCell.profileImage4.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
-       self.EventCell.profileImage4.clipsToBounds = true
-       self.EventCell.profileImage4.layer.borderWidth = 3.0
-       self.EventCell.profileImage4.layer.borderColor = UIColor(red:0.71, green:0.10, blue:0.11, alpha:1.0).cgColor
-       self.EventCell.profileImage5.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
-       self.EventCell.profileImage5.clipsToBounds = true
-       self.EventCell.profileImage5.layer.borderWidth = 3.0
-       self.EventCell.profileImage5.layer.borderColor = UIColor(red:0.71, green:0.10, blue:0.11, alpha:1.0).cgColor
-       self.EventCell.profileImage6.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
-       self.EventCell.profileImage6.clipsToBounds = true
-       self.EventCell.profileImage6.layer.borderWidth = 3.0
-       self.EventCell.profileImage6.layer.borderColor = UIColor(red:0.71, green:0.10, blue:0.11, alpha:1.0).cgColor
-       self.EventCell.profileImage6.layer.cornerRadius = self.EventCell.profileImage1.frame.size.width / 2
-       
-        
-
-        
-
-        
-
-
-      
-
-
-
-        
-
-
-
+       sizeUp()
        print("cell for row" )
        return EventCell
     }
