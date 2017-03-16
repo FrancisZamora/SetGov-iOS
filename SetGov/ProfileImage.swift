08aa0fdfb41b883cd2e131c49fc00acb3f74090d
FrancisZamora <franciszamor@gmail.com>
2017-03-15 20:48:12 -0400

    adding files for each UI element

---
+++ SetGov/ProfileImage.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileImage.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 3/15/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+
+class ProfilePicture: UIImageView {
+    didSet{
+    
+    }
+}
