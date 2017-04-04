39c0485c5b9cca03cf08e1a82b0dd7a721662245
FrancisZamora <franciszamor@gmail.com>
2017-04-03 23:03:50 -0400

    adding outlets

---
+++ SetGov/AgendaDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AgendaDetailViewController.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 4/3/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+
+class AgendaDetailViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var primaryImage: UIImageView!
+    
+    @IBOutlet var imageshadowView: UIView!
+}
