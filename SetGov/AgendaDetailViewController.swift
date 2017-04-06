147d02f2bf339d9a02e589a9d02425df9022e358
FrancisZamora <franciszamor@gmail.com>
2017-04-06 19:44:53 -0400

    delegate methods for  agenda detail

--- SetGov/AgendaDetailViewController.swift
+++ SetGov/AgendaDetailViewController.swift
@@ -10,7 +10,83 @@ import Foundation
 import UIKit
 import QuartzCore
 
-class AgendaDetailViewController: UITableViewController {
+class AgendaDetailViewController: SetGovTableViewController {
+    var  numsections = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("EventDetailViewController")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 50.0
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        self.numsections = 1
+        print("numberofSections")
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberofRows")
+        return 6
+        
+    }
+    
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        switch indexPath.row {
+        case 0:
+            return 200
+        case 1:
+            return 38
+        case 2:
+            return 176
+        case 3:
+            return 94
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        
+        
+        if (indexPath.row == 0) {
+            let agendadetailHeader =  tableView.dequeueReusableCell(withIdentifier: "AgendaDetailHeader", for:indexPath) as! AgendaDetailHeader
+            
+            
+            print("cell for row" )
+            return agendadetailHeader
+        }
+        
+        if(indexPath.row == 1) {
+            let agendadetailData =  tableView.dequeueReusableCell(withIdentifier: "AgendaDetailHeader", for:indexPath) as! AgendaDetailHeader
+            return agendadetailData
+        }
+        
+        if(indexPath.row == 2) {
+            let agendadetailComments = tableView.dequeueReusableCell(withIdentifier: "AgendaDetailComments", for:indexPath) as! AgendaDetailComments
+            
+            return agendadetailComments
+        }
+        let agendadetailHeader =  tableView.dequeueReusableCell(withIdentifier: "AgendaDetailHeader", for:indexPath) as! AgendaDetailHeader
+        
+        return agendadetailHeader
+        
+    }
+
     
     
 
