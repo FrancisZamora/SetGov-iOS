fe3f27e4d7dd8296956ac05d58451ce1d71c2736
FrancisZamora <franciszamor@gmail.com>
2017-04-11 23:38:01 -0400

    adding webview for Event

---
+++ SetGov/EventLiveStream.swift
@@ -0,0 +1,20 @@
+//
+//  EventLiveStream.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 4/11/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+
+class LiveStream: UITableViewCell {
+    
+    
+    
+    
+    
+    
+}
