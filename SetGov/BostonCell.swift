6bfc956f1cfdc1eab4881311f95bb4742b00cdbc
FrancisZamora <franciszamor@gmail.com>
2017-04-23 23:37:05 -0400

    added classes and supporting files

---
+++ SetGov/BostonCell.swift
@@ -0,0 +1,17 @@
+//
+//  BostonCell.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 4/23/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+
+
+class BostonCell: UITableViewCell {
+    
+    
+}
---
+++ SetGov/NewYorkCityCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewYorkCityCell.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 4/23/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+
+class NewYorkCityCell: UITableViewCell {
+    
+    
+}
