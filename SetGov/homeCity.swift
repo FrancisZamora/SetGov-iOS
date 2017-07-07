f3ee811cee1f31ca30f5867f3c197bfb138d350c
FrancisZamora <franciszamor@gmail.com>
2017-07-07 15:23:40 -0400

    working on settings and profile

---
+++ SetGov/homeCity.swift
@@ -0,0 +1,18 @@
+//
+//  homeCity.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 7/7/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class homeCity: UITableViewCell {
+
+
+
+
+
+}
