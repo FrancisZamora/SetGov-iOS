241b58e60480b7dd8266a6e3c2fe766cd5913306
FrancisZamora <franciszamor@gmail.com>
2017-07-07 11:29:04 -0400

    working on settings

---
+++ SetGov/SettingsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsTableViewController.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 7/7/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: SetGovTableViewController {
+    
+    
+}
