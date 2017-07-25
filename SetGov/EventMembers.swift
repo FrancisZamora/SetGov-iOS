ca882995ea34a39a77d9ff8fb28c68afeb08e8e5
FrancisZamora <franciszamor@gmail.com>
2017-07-25 12:48:07 -0400

    picture showing for attending event

--- SetGov/EventMembers.swift
+++ SetGov/EventMembers.swift
@@ -17,6 +17,7 @@ class EventMembers: UITableViewCell {
     
     @IBOutlet var profilePicture1: ProfilePicture!
     @IBOutlet var profilePicture2: ProfilePicture!
+    @IBOutlet var conView: UIView!
     @IBOutlet var profilePicture3: ProfilePicture!
     @IBOutlet var profilePicture4: ProfilePicture!
     @IBOutlet var profilePicture5: ProfilePicture!
@@ -24,11 +25,34 @@ class EventMembers: UITableViewCell {
     
     
     func checkMembers() {
+        profilePicture1.isHidden = true
+        
         ApiClient.fetchEvent(eventID:bostonIDS[indexofEvent] , onCompletion:{ json in
             
             let pictureIDArray = json["data"]["event"]["attendingUsers"].arrayValue.map({$0["profileImage"]["url"].stringValue})
             print(pictureIDArray)
-            
+            for (index,_) in pictureIDArray.enumerated() {
+                var xy = 8
+                var imageView : UIImageView
+                self.imageView?.layer.cornerRadius = self.frame.height / 2
+                self.imageView?.clipsToBounds = true
+                self.imageView?.layer.borderWidth = 1.5
+                self.imageView?.layer.borderColor = UIColor(red:0.18, green:0.26, blue:0.35, alpha:1.0).cgColor
+                
+
+                imageView  = UIImageView(frame:CGRect(x: xy, y:42, width:35, height:35))
+                let theProfileImageUrl = URL(string:pictureIDArray[index])
+                do {
+                    let imageData = try Data(contentsOf: theProfileImageUrl!)
+                    imageView.image = UIImage(data: imageData)
+                } catch {
+                    print("Unable to load data: \(error)")
+                }
+               
+
+                self.conView.addSubview(imageView)
+                xy = xy + 48
+            }
             
             
             
