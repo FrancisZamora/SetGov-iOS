0b8e750ed09b92e4e8c94e72aa2f468f6e0419ac
FrancisZamora <franciszamor@gmail.com>
2017-07-27 11:11:40 -0400

    adding ui collection view cell class

---
+++ SetGov/Attendee.swift
@@ -0,0 +1,15 @@
+//
+//  Attendee.swift
+//  SetGov
+//
+//  Created by Francis Zamora on 7/27/17.
+//  Copyright © 2017 Francis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Attendee: UICollectionViewCell {
+    
+    
+}
