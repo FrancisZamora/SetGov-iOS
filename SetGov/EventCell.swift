a2908f589e1b61d8aa39d57bd561b563768d7a3d
Balin Sinnott <balin@carolina-pc.afus.altamarfoods.local>
2017-08-11 12:25:20 -0400

    removed no users collection cell.  Added no users hidden view.

--- SetGov/EventCell.swift
+++ SetGov/EventCell.swift
@@ -33,7 +33,6 @@ class EventCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewData
     var spacer = "  "
     var picArray = [String]()
     var event: Event!
-    var userArray = [User]()
 
     @IBOutlet var attendee: ProfilePicture!
     
@@ -43,18 +42,11 @@ class EventCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewData
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if event.users.count == 0 {
-            return 1
-        }
         return event.users.count
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if event.users.count == 0 {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NoUsers", for: indexPath) as! NoUsers
-            return cell
-        }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Attendee", for: indexPath) as! Attendee
         
         print(event.users)
@@ -72,13 +64,17 @@ class EventCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewData
         eventDescription.text = event.description
         eventImage.image = event.image
         eventDate.text = event.eventDate
-        self.userArray = event.users
         memberCount.text = "\(event.users.count)"
-      
         self.event = event
-        print(event.users)
-        self.usersCollection.reloadData()
+        
+        if(event.users.count > 0) {
+            self.usersCollection.reloadData()
+        } else {
+            usersCollection.isHidden = true
+        }
+        
         
     }
-  
+    
+    
 }
